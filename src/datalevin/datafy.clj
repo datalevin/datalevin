9e30e89e66cac0204799c9ce3bbd021836df8094
Huahai Yang <huahai.yang@gmail.com>
2026-06-24 19:59:30 -0700

    resolve #292

--- src/datalevin/datafy.clj
+++ src/datalevin/datafy.clj
@@ -10,30 +10,79 @@
 (ns ^:no-doc datalevin.datafy
   "Implement Clojure Datafiable protocol for entities"
   (:require [clojure.core.protocols :as cp]
+            [clojure.set :as set]
             [datalevin.pull-api :as dp]
             [datalevin.db :as db]
             [datalevin.entity :as e]))
 
 (declare datafy-entity-seq)
 
+(defn- attr-types [db-val]
+  (let [rschema          (db/-rschema db-val)
+        ref-attrs        (set (rschema :db.type/ref))
+        many-attrs       (set (rschema :db.cardinality/many))
+        component-attrs  (set (rschema :db/isComponent))
+        ref-many-rattrs  (into #{}
+                                (map db/reverse-ref)
+                                (set/difference ref-attrs component-attrs))
+        component-rattrs (into #{} (map db/reverse-ref) component-attrs)]
+    {:ref-attrs        ref-attrs
+     :many-attrs       many-attrs
+     :ref-many-rattrs  ref-many-rattrs
+     :component-rattrs component-rattrs}))
+
+(declare normalize-pulled-entity)
+
+(defn- many-values [v]
+  (if (and (coll? v) (not (map? v))) v [v]))
+
+(defn- normalize-ref-value [types v]
+  (if (map? v) (normalize-pulled-entity types v) v))
+
+(defn- normalize-pulled-value
+  [{:keys [ref-attrs many-attrs ref-many-rattrs component-rattrs] :as types} k v]
+  (cond
+    (nil? v) nil
+
+    (or (and (many-attrs k) (ref-attrs k))
+        (ref-many-rattrs k))
+    (into #{} (map #(normalize-ref-value types %)) (many-values v))
+
+    (many-attrs k)
+    (set (many-values v))
+
+    (or (ref-attrs k) (component-rattrs k))
+    (normalize-ref-value types v)
+
+    :else v))
+
+(defn- normalize-pulled-entity [types pulled-entity]
+  (persistent!
+    (reduce-kv
+      (fn [m k v]
+        (assoc! m k (normalize-pulled-value types k v)))
+      (transient {})
+      pulled-entity)))
+
 (defn- navize-pulled-entity [db-val pulled-entity]
-  (let [rschema    (db/-rschema db-val)
-        ref-attrs  (rschema :db.type/ref )
-        ref-rattrs (set (map db/reverse-ref ref-attrs))
-        many-attrs (rschema :db.cardinality/many)]
+  (let [{:keys [ref-attrs many-attrs ref-many-rattrs component-rattrs]
+         :as   types} (attr-types db-val)
+        pulled-entity (normalize-pulled-entity types pulled-entity)]
     (with-meta pulled-entity
-      {`cp/nav (fn [coll k v]
+      {`cp/nav (fn [_coll k v]
                  (cond
                    (or (and (many-attrs k) (ref-attrs k))
-                       (ref-rattrs k))
+                       (ref-many-rattrs k))
                    (datafy-entity-seq db-val v)
+                   (component-rattrs k)
+                   (e/entity db-val (:db/id v))
                    (ref-attrs k)
                    (e/entity db-val (:db/id v))
                    :else v))})))
 
 (defn- navize-pulled-entity-seq [db-val entities]
   (with-meta entities
-    {`cp/nav (fn [coll k v]
+    {`cp/nav (fn [_coll _k v]
                (e/entity db-val (:db/id v)))}))
 
 (defn- datafy-entity-seq [db-val entities]
